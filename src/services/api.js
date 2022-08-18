2f20f5333ad4c89ae652e130eafff7f16619501d
dambar <notdambar@gmail.com>
2022-08-18 12:30:48 +0800

    init

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const BASE_URL = process.env.API_BASE_URL;
 
 export const getIssues = async () => {
-  const response = await fetch(`$BASE_URL}/api/issues`);
+  const response = await fetch(`${BASE_URL}/api/issues`);
   return await response.json();
 };
